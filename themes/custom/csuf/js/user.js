53a5c65a4f523a28857450fe09f9a5a3785998f6
augustpascual-mse <augustpascual.mse@gmail.com>
2016-04-17 21:20:10 -0700

    Update Profile Page

--- themes/custom/csuf/js/user.js
+++ themes/custom/csuf/js/user.js
@@ -1,4 +1,9 @@
 jQuery(function ($){
     var candidateName = $('.field--name-field-first-name .field__item').text() + ' ' + $('.field--name-field-last-name .field__item').text();
     $('.personal-information .title').text(candidateName);
+
+    $('#edit-field-school-start-date-0-value-day').val('1');
+    $('#edit-field-school-end-date-0-value-day').val('1');
+    $('#edit-field-job-start-date-0-value-day').val('1');
+    $('#edit-field-job-end-date-0-value-day').val('1');
 });
